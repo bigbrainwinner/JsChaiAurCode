c45961621793b55197f6c17b071551b611fd3564
bigbrainwinner <141313205+bigbrainwinner@users.noreply.github.com>
2024-04-03 09:47:12 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456
+let accountEmail = "Dhanush@gmail.com"
+accountEmail = "Kichudhanush@gmail.com"
+let accountState;
+
+console.table([ accountEmail, accountId, accountState]);
+// please dont use var, because of issue in block scope and func scope
---
+++ 01_basics/0_Datatypes.js
@@ -0,0 +1,16 @@
+let name = "Dhanush"
+let age = 20
+let isLoggedIn = false
+
+
+// number=> 2 to power 53
+// BigInt
+// String=> ""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbols=> used to define and unqiue
+
+// object
+
+console.log(typeof age);
